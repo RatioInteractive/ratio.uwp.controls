4cb15dd9f6a2ed6f585010679dd908f6e054197d
Mike McAulay <mmcaulay@weareratio.com>
2018-03-01 13:35:08 -0800

    RTVP-1382

--- BaseItem.cs
+++ BaseItem.cs
@@ -3,13 +3,18 @@ using System.Windows.Input;
 using Windows.System;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
 
 namespace Ratio.UWP.Controls
 {
+    [TemplatePart(Name = "listViewItemPresenter", Type = typeof(ListViewItemPresenter))]
     public class BaseItem :GridViewItem
     {
+        private ListViewItemPresenter _listViewItemPresenter;
+        private IContainerFocusable _focusable;
         public Binding ItemBinding { get; set; }
         public static readonly DependencyProperty SelectedCommandProperty = DependencyProperty.Register(
             "SelectedCommand", typeof(ICommand), typeof(BaseItem), new PropertyMetadata(default(ICommand)));
@@ -20,6 +25,18 @@ namespace Ratio.UWP.Controls
             set => SetValue(SelectedCommandProperty, value);
         }
         public object SourceItem { get; set; }
+
+        private IContainerFocusable Focusable
+        {
+            get
+            {
+                if (_focusable != null) return _focusable;
+                if (_listViewItemPresenter == null) return null;
+                if (!(VisualTreeHelper.GetChild(_listViewItemPresenter,0) is IContainerFocusable child)) return null;
+                _focusable = child;
+                return _focusable;
+            }
+        }
 
         protected override void OnTapped(TappedRoutedEventArgs e)
         {
@@ -46,6 +63,24 @@ namespace Ratio.UWP.Controls
                 }
             }
             base.OnKeyUp(e);
+        }
+
+        protected override void OnGotFocus(RoutedEventArgs e)
+        {
+            Focusable?.ContainerGotFocus();
+            base.OnGotFocus(e);
+        }
+
+        protected override void OnLostFocus(RoutedEventArgs e)
+        {
+            Focusable?.ContainerLostFocus();
+            base.OnLostFocus(e);
+        }
+
+        protected override void OnApplyTemplate()
+        {
+            _listViewItemPresenter = GetTemplateChild("listViewItemPresenter") as ListViewItemPresenter;
+            base.OnApplyTemplate();
         }
     }
 }
